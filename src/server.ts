78dbcc6bcab1441fe297ca3f773631c563066cef
Parvez141300 <parvezhossain744471@gmail.com>
2026-03-12 17:30:30 +0600

    created a env.ts file to import easily from any where because in process.env it does not give suggestion

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import app from "./app"
+import { envVars } from "./config/env";
 
 
 const bootstrap = () => {
     try {
-        app.listen(5000, () => {
-            console.log(`Server app is listening on port ${5000}`);
+        app.listen(envVars.PORT, () => {
+            console.log(`Server app is listening on port ${envVars.PORT}`);
         });
     } catch (error) {
         console.log('failed to start server', error);
