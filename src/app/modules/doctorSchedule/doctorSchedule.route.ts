32829dc13a137e8732d722ab37ad9631b8296c0b
Parvez141300 <parvezhossain744471@gmail.com>
2026-04-27 17:53:36 +0600

    updated the doctor schedule patch api route controller

--- src/app/modules/doctorSchedule/doctorSchedule.route.ts
+++ src/app/modules/doctorSchedule/doctorSchedule.route.ts
@@ -9,7 +9,7 @@ router.get("/", DoctorScheduleController.getAllDoctorSchedule);
 router.get("/my-doctor-schedules", DoctorScheduleController.getMyDoctorSchedule);
 router.get("/:doctorId/schedule/:scheduleId", DoctorScheduleController.getDoctorScheduleById);
 router.post("/create-my-doctor-schedule", checkAuth(Role.DOCTOR), DoctorScheduleController.createDoctorSchedule);
-router.patch("/update-my-doctor-schedule", checkAuth(Role.DOCTOR), DoctorScheduleController.getDoctorScheduleById);
+router.patch("/update-my-doctor-schedule", checkAuth(Role.DOCTOR), DoctorScheduleController.updateMyDoctorSchedule);
 router.delete("/delete-my-doctor-schedule/:id", checkAuth(Role.DOCTOR), DoctorScheduleController.deleteMyDoctorSchedule);
 
 export const DoctorScheduleRoute = router;
